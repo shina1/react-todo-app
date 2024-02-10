ddf8760ce91a78316fe63883b32c840cce09175d
shina1 <oswasiu@gmail.com>
2024-02-10 11:33:23 +0100

    project starter files

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+// import React from 'react';
+import TodoListTitle from './component/TodoListTitle';
+import TodoContainer from './component/TodoListTitle';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="container">
+      <TodoListTitle />
+      <TodoContainer />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/component/TodoListTitle.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+// import './todotitle';
+
+const TodoListTitle = () => {
+  return (
+    <div>
+      <h1>todos</h1>
+      <p>Item will persist in the browser local storage</p>
+    </div>
+  );
+};
+
+export default TodoListTitle;
